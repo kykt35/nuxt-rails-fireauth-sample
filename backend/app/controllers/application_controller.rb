7b2b819bcc96bbbbbb28026ea67ca1e263d3131d
kykt35 <kykt35@gmail.com>
2019-10-02 18:44:11 +0900

    Add authenticate method to applicationController

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::API
+
+
+  def current_user
+    @current_user
+  end
+
+  private
+
+  def authenticate_by_fireauth!
+    begin
+      binding.pry
+      decoded_token = FireAuthUtil.verify_id_token!(http_token)
+      payload = decoded_token[:payload]
+      @current_user = User.find_or_create_by!(fireauth_uid: payload["sub"])
+
+    rescue => e
+      render json: { errors: ['Not Authenticated'], message: e }, status: :unauthorized
+    end
+  end
+
+  def http_token
+    if request.headers['Authorization'].present?
+      request.headers['Authorization'].split(' ').last
+    end
+  end
+
 end
