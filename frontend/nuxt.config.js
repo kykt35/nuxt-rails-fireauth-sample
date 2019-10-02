46392dcdcb522d42f67b35a7ebf9f30ec4a6ffa3
kykt35 <kykt35@gmail.com>
2019-10-02 14:44:32 +0900

    Update nuxt.config to add plugin and env

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -30,6 +30,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '~/plugins/firebase',
   ],
   /*
   ** Nuxt.js dev-modules
@@ -44,6 +45,16 @@ export default {
   */
   modules: [
   ],
+
+  env: {
+    FB_API_KEY: process.env.FB_API_KEY,  
+    FB_AUTH_DOMAIN: process.env.FB_AUTH_DOMAIN,
+    FB_DATABASE_URL: process.env.FB_DATABASE_URL, 
+    FB_PROJECTID: process.env.FB_PROJECTID,
+    FB_STORAGE_BUCKET: process.env.FB_STORAGE_BUCKET, 
+    FB_MESSAGING_SENDER_ID: process.env.FB_MESSAGING_SENDER_ID,
+    FB_APP_ID: process.env.FB_APP_ID,
+  },
   /*
   ** vuetify module configuration
   ** https://github.com/nuxt-community/vuetify-module
